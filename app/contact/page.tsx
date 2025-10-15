b83c906ccf1dcab1b6a3b1ad1a0b6dd5d82c245e
Muhamad Iqbal <muhamadiqbalfalah864@gmail.com>
2025-10-15 15:37:30 +0700

    add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,71 @@
+import { Metadata } from "next";
+import HeaderSsection from "@/components/header-section";
+import {
+  IoMaleOutline,
+  IoCallOutline,
+  IoLocationOutline,
+} from "react-icons/io5";
+
+export const metadata: Metadata = {
+  title: "Contact",
+};
+
+const ContactPage = () => {
+  return (
+    <div>
+      <HeaderSsection
+        title="Conatct Us"
+        subTitle="Lorem ipsum dolor sit amet."
+      />
+      <div className="max-w-screen-xl mx-auto py-20 px-4">
+        <div className="grid md:grid-cols-2 gap-8">
+          <div>
+            <h1 className="text-lg text-gray-500 mb-3">Contact Us</h1>
+            <h1 className="text-5xl font-semibold text-gray-900 mb-4">
+              Get In Touch Today
+            </h1>
+            <p className="text-gray-700 py-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptate, error officiis quis id aut adipisci!
+            </p>
+            <ul className="list-item space-y-6 pt-8">
+              <li className="flex gap-5">
+                <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
+                  <IoMaleOutline className="size-7" />
+                </div>
+                <div className="flex-1">
+                  <h4 className="text-lg font-semibold mb-1">Email</h4>
+                  <p>Email@example.com</p>
+                </div>
+              </li>
+              <li className="flex gap-5">
+                <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
+                  <IoCallOutline className="size-7" />
+                </div>
+                <div className="flex-1">
+                  <h4 className="text-lg font-semibold mb-1">Phone Number</h4>
+                  <p>+62087899878, +6287897898</p>
+                </div>
+              </li>
+              <li className="flex gap-5">
+                <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
+                  <IoLocationOutline className="size-7" />
+                </div>
+                <div className="flex-1">
+                  <h4 className="text-lg font-semibold mb-1">Address</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Fugit, fugiat.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          {/* Contact Form */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
